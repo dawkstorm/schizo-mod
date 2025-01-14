5d08b505384c5a3e44efc5d09cc0bf4b879468e7
dawkstorm <john.kalmykov@gmail.com>
2025-01-14 11:05:37 +0100

    first commit

---
+++ src/main/java/net/dawkstorm/schizo/SchizoClient.java
@@ -0,0 +1,10 @@
+package net.dawkstorm.schizo;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SchizoClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/dawkstorm/schizo/SchizoDataGenerator.java
@@ -0,0 +1,11 @@
+package net.dawkstorm.schizo;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class SchizoDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
