caef94522082e99e4bfc7683a6e0d63704f7beca
dawkstorm <john.kalmykov@gmail.com>
2025-01-15 13:56:22 +0100

    zombie ambient sound

--- src/main/java/net/dawkstorm/schizo/Schizo.java
+++ src/main/java/net/dawkstorm/schizo/Schizo.java
@@ -1,21 +1,31 @@
-package net.dawkstorm.schizo;
+	package net.dawkstorm.schizo;
 
-import net.fabricmc.api.ModInitializer;
+	import net.fabricmc.api.ModInitializer;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+	import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+	import net.fabricmc.fabric.api.event.player.AttackBlockCallback;
+	import net.fabricmc.fabric.api.event.player.AttackEntityCallback;
+	import net.minecraft.client.MinecraftClient;
+	import net.minecraft.sound.SoundCategory;
+	import net.minecraft.sound.SoundEvents;
+	import net.minecraft.util.ActionResult;
+	import org.slf4j.Logger;
+	import org.slf4j.LoggerFactory;
 
-public class Schizo implements ModInitializer {
-	public static final String MOD_ID = "schizomod";
-	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
-	@Override
-	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
+	public class Schizo implements ModInitializer {
+		public static final String MOD_ID = "schizo";
+		public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
-		LOGGER.info("meow :3");
+		@Override
+		public void onInitialize() {
+			LOGGER.info("meow :3");
+			ClientTickEvents.START_WORLD_TICK.register(client -> {
+				if(Math.random() < 0.005){
+					LOGGER.info("hehehee");
+					client.getPlayers().getFirst().playSound(SoundEvents.ENTITY_ZOMBIE_AMBIENT);
+				}
+			});
+		}
+
 	}
-
-}
--- src/main/java/net/dawkstorm/schizo/SchizoClient.java
+++ src/main/java/net/dawkstorm/schizo/SchizoClient.java
@@ -1,6 +1,9 @@
 package net.dawkstorm.schizo;
 
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.minecraft.sound.SoundCategory;
+import net.minecraft.sound.SoundEvents;
 
 public class SchizoClient implements ClientModInitializer {
     @Override
